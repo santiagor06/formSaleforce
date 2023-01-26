fe16dfe822894fd61fc6480df16f2bacd7cfdf7e
santiagor06 <santiago.restrepo06@hotmail.com>
2023-01-25 21:58:51 -0500

    form HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState({ orgid: "00DDn00000A9xuh", retURL: "" });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form
+        action="https://webto.salesforce.com/servlet/servlet.WebToCase"
+        method="POST"
+      >
+        <input name="orgid" value="00DDn00000A9xuh" />
+        <input name="retURL" value="http://localhost:3000/" />
+
+        <label for="name">Nombre del contacto</label>
+        <input id="name" maxlength="80" name="name" size="20" type="text" />
+        <br />
+
+        <label for="email">Correo electrónico</label>
+        <input id="email" maxlength="80" name="email" size="20" type="text" />
+        <br />
+
+        <label for="phone">Teléfono</label>
+        <input id="phone" maxlength="40" name="phone" size="20" type="text" />
+        <br />
+
+        <label for="subject">Asunto</label>
+        <input
+          id="subject"
+          maxlength="80"
+          name="subject"
+          size="20"
+          type="text"
+        />
+        <br />
+
+        <label for="description">Descripción</label>
+        <textarea name="description"></textarea>
+        <br />
+
+        <input type="hidden" id="external" name="external" value="1" />
+        <br />
+
+        <input type="submit" name="submit" />
+      </form>
     </div>
   );
 }
